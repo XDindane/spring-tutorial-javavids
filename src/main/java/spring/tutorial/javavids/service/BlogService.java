00b9b677c0d717af96c9b1f80e49c347a0a65e18
Denis Mahmúd <denis.mahmud@gmail.com>
2016-01-02 16:13:12 +0100

    Tut. 33 Spring Security PreAuthorize

--- src/main/java/spring/tutorial/javavids/service/BlogService.java
+++ src/main/java/spring/tutorial/javavids/service/BlogService.java
@@ -1,6 +1,8 @@
 package spring.tutorial.javavids.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import spring.tutorial.javavids.entity.Blog;
 import spring.tutorial.javavids.entity.User;
@@ -24,11 +26,16 @@ public class BlogService {
         blogRepository.save(blog);
         
     }
+    
+    @PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
+    public void delete(@P("blog") Blog blog) {
+        blogRepository.delete(blog);
+        
+        
+    }
 
-    public void delete(int id) {
-        blogRepository.delete(id);
-        
-        
+    public Blog findOne(int id) {
+        return blogRepository.findOne(id);
     }
     
 }
